14f8f17154971b0218b11cdedfe7fefa0619e51f
Rounack Kundu <rounack1@gmail.com>
2021-10-12 00:52:37 +0530

    controller

--- FlightBooking-1/src/main/java/com/cg/controller/BookingController.java
+++ FlightBooking-1/src/main/java/com/cg/controller/BookingController.java
@@ -1,5 +1,33 @@
 package com.cg.controller;
 
-public class BookingController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.cg.Dto.BookingDetailsDto;
+import com.cg.exceptions.BookingExceptions;
+import com.cg.exceptions.ScheduleException;
+import com.cg.exceptions.UserException;
+import com.cg.model.BookingDetails;
+import com.cg.service.BookingDetailsService;
+
+@RestController
+public class BookingController {
+	
+	@Autowired
+	private BookingDetailsService bookingService;
+	
+	@PostMapping("/addbooking")
+    public BookingDetails addBooking(@RequestBody BookingDetailsDto bookingDto, BindingResult br ) throws UserException, ScheduleException, BookingExceptions
+    {
+		if(br.hasErrors())
+		{
+			throw new BookingExceptions(br.getFieldErrors());
+		}
+        BookingDetails bookings = bookingService.addBooking(bookingDto);
+        return bookings;
+    }
+	
 }
--- FlightBooking-1/src/main/java/com/cg/exceptions/BookingExceptions.java
+++ FlightBooking-1/src/main/java/com/cg/exceptions/BookingExceptions.java
@@ -1,14 +1,30 @@
 package com.cg.exceptions;
 
+import java.util.List;
+
+import org.springframework.validation.FieldError;
+
 public class BookingExceptions extends Exception{
 	
 	
 
 	public BookingExceptions(String message) {
 		super(message);
+		this.errors = null;
 	}
 
 	public BookingExceptions() {
+		this.errors = null;
 	}
+	public final List<FieldError> errors;
+	public BookingExceptions(List<FieldError> errors) {
+		super();
+		this.errors = errors;
+	}
+
+	public List<FieldError> getErrors() {
+		return errors;
+	}
+	
 
 }
