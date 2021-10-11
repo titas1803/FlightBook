011cbf3ff4126d93ce50b8b7f933e91f6992ff27
titas1803 <titas.sarkar420@hotmail.com>
2021-10-12 01:02:28 +0530

    success & error message...

---
+++ FlightBooking-1/src/main/java/com/cg/util/LoginResponse.java
@@ -0,0 +1,37 @@
+package com.cg.util;
+
+public class LoginResponse {
+
+	private Integer userId;
+	private String token;
+	private String userName;
+	private String role;
+	
+	
+	public Integer getUserId() {
+		return userId;
+	}
+	public void setUserId(Integer userId) {
+		this.userId = userId;
+	}
+	public String getToken() {
+		return token;
+	}
+	public void setToken(String token) {
+		this.token = token;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
+	
+	
+}
