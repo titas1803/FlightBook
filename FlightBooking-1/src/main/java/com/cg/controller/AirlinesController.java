ec062952226d3369db5b6aef35a178cac4809366
Rounack Kundu <rounack1@gmail.com>
2021-10-12 01:41:07 +0530

    airline and booking controller

--- FlightBooking-1/src/main/java/com/cg/controller/AirlinesController.java
+++ FlightBooking-1/src/main/java/com/cg/controller/AirlinesController.java
@@ -1,4 +1,48 @@
 package com.cg.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cg.Dto.AirlinesDetailsDto;
+import com.cg.exceptions.AirlineExceptions;
+import com.cg.model.AirlineDetails;
+import com.cg.service.AirlinesDetailsService;
+import com.cg.util.SuccessMessage;
+
+@RestController
 public class AirlinesController {
+	
+	@Autowired
+	private AirlinesDetailsService airlineService;
+	
+	@PostMapping("/addairline")
+	public AirlineDetails addAirline(@RequestBody AirlinesDetailsDto airlineDto, BindingResult br) throws AirlineExceptions
+	{
+		if(br.hasErrors())
+		{
+			throw new AirlineExceptions();
+		}
+		AirlineDetails airlines = airlineService.addAirline(airlineDto);
+		return airlines;
+	}
+	
+	@DeleteMapping("/deleteairline/{id}")
+	public SuccessMessage deleteAirline(@PathVariable("id") Integer airlineId) throws AirlineExceptions
+	{
+		return new SuccessMessage(airlineService.deleteAirline(airlineId));
+	}
+	
+	@GetMapping("/getallairlines")
+	public List<AirlineDetails> viewAllAirlines() throws AirlineExceptions
+	{
+		return airlineService.viewAllAirlines();
+	}
 }
--- FlightBooking-1/src/main/java/com/cg/controller/BookingController.java
+++ FlightBooking-1/src/main/java/com/cg/controller/BookingController.java
@@ -1,7 +1,12 @@
 package com.cg.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +17,8 @@ import com.cg.exceptions.ScheduleException;
 import com.cg.exceptions.UserException;
 import com.cg.model.BookingDetails;
 import com.cg.service.BookingDetailsService;
+import com.cg.util.FlightBookingConstants;
+import com.cg.util.SuccessMessage;
 
 @RestController
 public class BookingController {
@@ -29,5 +36,23 @@ public class BookingController {
         BookingDetails bookings = bookingService.addBooking(bookingDto);
         return bookings;
     }
+	
+	@DeleteMapping("/deletebooking/{id}")
+	public SuccessMessage deletebooking(@PathVariable("id") Integer ticketid) throws BookingExceptions
+	{
+		return new SuccessMessage(bookingService.deleteBooking(ticketid));
+	}
+	
+	@GetMapping("/getbookingbyid/{id}")
+	public BookingDetails viewBookingById(@PathVariable("id") Integer ticketId) throws BookingExceptions
+	{
+		return bookingService.viewById(ticketId);
+	}
+	
+	@GetMapping("/getallbookings")
+	public List<BookingDetails> viewAllBookings() throws BookingExceptions
+	{
+		return bookingService.viewAllBookings();
+	}
 	
 }
