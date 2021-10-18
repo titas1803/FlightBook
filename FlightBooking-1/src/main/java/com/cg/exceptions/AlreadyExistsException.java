c9a6c5ff4957b5a83128757671fa70d9b7bc1027
titas1803 <titas.sarkar420@hotmail.com>
2021-10-18 18:17:27 +0530

    Exceptions Created

---
+++ FlightBooking-1/src/main/java/com/cg/exceptions/AlreadyExistsException.java
@@ -0,0 +1,17 @@
+package com.cg.exceptions;
+
+public class AlreadyExistsException extends Exception {
+
+	public AlreadyExistsException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public AlreadyExistsException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+
+}
---
+++ FlightBooking-1/src/main/java/com/cg/exceptions/NotFoundException.java
@@ -0,0 +1,16 @@
+package com.cg.exceptions;
+
+public class NotFoundException extends Exception {
+
+	public NotFoundException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public NotFoundException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+	
+
+}
